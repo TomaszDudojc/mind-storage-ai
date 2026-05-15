22e6a70feeea09f689b393cb0955918a3c743852
Tomasz Dudojć <tomek.dudojc@gmail.com>
2026-05-15 10:07:15 +0200

    style: update logo and app title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
-import EditNoteIcon from '@mui/icons-material/EditNote';
+//import EditNoteIcon from '@mui/icons-material/EditNote';
+import { LuBrainCircuit } from "react-icons/lu"; // Mózg z liniami obwodów drukowanych (bardzo technologiczny/AI)
 import SettingsPowerIcon from '@mui/icons-material/SettingsPower';
 
-function Header(props) {  
-  function handleLogout(){   
+function Header(props) {
+  function handleLogout() {
     localStorage.clear();
     window.location.reload();
   }
 
   return (
     <header>
-      <h1><EditNoteIcon fontSize="large"/> Note Storage {props.isLogged && <button className="logoutButton" onClick={handleLogout}><SettingsPowerIcon style={{fontWeight: 700, fontSize: "46px"}}/></button>}</h1>
+      <h1><LuBrainCircuit className="ai-glow-icon" /> Mind Storage AI{props.isLogged && <button className="logoutButton" onClick={handleLogout}><SettingsPowerIcon style={{ fontWeight: 700, fontSize: "46px" }} /></button>}</h1>
     </header>
-  ); 
+  );
 }
 
 export default Header;
